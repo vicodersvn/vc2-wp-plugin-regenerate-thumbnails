7b96d5c030cb5185a8b2c14800d71d7ed8b46b48
taeung123 <youtanlai@gmail.com>
2020-03-02 11:34:38 +0700

    add tag

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
-import { Rule, chain, apply, url, move, mergeWith, applyTemplates } from '@angular-devkit/schematics';
-import { strings } from '@angular-devkit/core';
-import { WordpressService } from './services/Php/Wordpress/WordpressService';
-import { App } from '@nsilly/container';
-import { appendTo } from './utility/append-to-file/append-to-file';
-import * as path from 'path';
+import { Rule, chain } from '@angular-devkit/schematics';
+import { common } from '@vicoders/cli-support';
 
-export default function handler(options: any): Rule {
-  const templateSource = apply(url('./files/slider'), [
-    applyTemplates({
-      ...strings,
-      ...options
-    }),
-    move(options.path)
-  ]);
-  const block_script = path.resolve(process.cwd(), 'resources', 'assets', 'scripts', 'blocks', 'index.js');
-  return chain([
-    mergeWith(templateSource),
-    App.make(WordpressService).declareInServiceProvider('app/Providers/BlockServiceProvider.php', `\\App\\Blocks\\SliderBlock\\SliderBlock::class,`),
-    appendTo(block_script, 'import "./_vc2_slider_block";')
-  ]);
+export default function handler(): Rule {
+  return chain([common.console.exec('vc2-wp plugin install regenerate-thumbnails')]);
 }
