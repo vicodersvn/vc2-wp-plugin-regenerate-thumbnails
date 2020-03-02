1c04db8dd3b6c28b63da1cb0868ef0800937801b
Hieupv <hieu-pv@users.noreply.github.com>
2020-03-02 13:06:57 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Rule, chain } from '@angular-devkit/schematics';
 import { common } from '@vicoders/cli-support';
 
-export default function handler(): Rule {
+export default function handler(_options?:any): Rule {
   return chain([common.console.exec('vc2-wp plugin install regenerate-thumbnails')]);
 }
