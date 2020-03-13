9b2ca0df0d520a7f50fd24d33b38aebd634d9404
taeung123 <youtanlai@gmail.com>
2020-03-13 09:37:01 +0700

    setup plugin

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Rule, chain } from '@angular-devkit/schematics';
 import { common } from '@vicoders/cli-support';
 
-export default function handler(): Rule {
-  return chain([common.console.exec('vc2-wp plugin install regenerate-thumbnails')]);
+export default function handler(options: any): Rule {
+  return chain([common.console.exec(`vc2-wp plugin install ${options.plugin}`)]);
 }
